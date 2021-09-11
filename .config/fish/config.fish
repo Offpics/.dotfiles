bc36dd21fad1554682fb092663961195dcbe4076
Offpics <kornelux1@gmail.com>
2021-09-11 23:08:25 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,5 @@ end
 alias config '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+source /opt/miniconda3/etc/fish/conf.d/conda.fish
